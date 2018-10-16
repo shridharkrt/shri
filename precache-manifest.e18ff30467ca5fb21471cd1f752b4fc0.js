dfb4242fef0a7bc647372dc448a73d89027b8534
shridharkrt <36045245+shridharkrt@users.noreply.github.com>
2018-10-17 01:42:14 +0530

    shop

---
+++ precache-manifest.e18ff30467ca5fb21471cd1f752b4fc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "334773f5f22a87dcd24c",
+    "url": "/static/js/main.334773f5.chunk.js"
+  },
+  {
+    "revision": "52b002c62265481cb373",
+    "url": "/static/js/1.52b002c6.chunk.js"
+  },
+  {
+    "revision": "334773f5f22a87dcd24c",
+    "url": "/static/css/main.a067dd3d.chunk.css"
+  },
+  {
+    "revision": "52b002c62265481cb373",
+    "url": "/static/css/1.f571dd3e.chunk.css"
+  },
+  {
+    "revision": "eeca638e5c44533c0083c2f29d7e3e32",
+    "url": "/index.html"
+  }
+];
